29428f7812438b7578dc913f9aa129fc00285101
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2021-06-20 07:09:00 +0000

    fix(ci): use caching in all tests

--- scripts/test_ci_dereplication.sh
+++ scripts/test_ci_dereplication.sh
@@ -1,2 +1,2 @@
-make run_small_full_test WORK_DIR="/mnt/work/work" OPTIONS=" --steps.dereplication.pasolli.input=test/bins/small/attributes.tsv " PROFILE="local"  ENTRY="wDereplication" PARAMS_FILE=example_params/dereplication.yml || exit 1
+make run_small_full_test CACHE="NXF_HOME=/mnt/nextflow_cache/"  WORK_DIR="/mnt/work/work" OPTIONS=" -process.cache='lenient' --steps.dereplication.pasolli.input=test/bins/small/attributes.tsv " PROFILE="local"  ENTRY="wDereplication" PARAMS_FILE=example_params/dereplication.yml || exit 1
 make check
--- scripts/test_ci_magAttributes.sh
+++ scripts/test_ci_magAttributes.sh
@@ -1,2 +1,2 @@
-make run_small_full_test WORK_DIR="/mnt/work/work" OPTIONS=" --steps.magAttributes.input=test/bins/small/attributes.tsv " PROFILE="local"  ENTRY="wMagAttributes" PARAMS_FILE=example_params/magAttributes.yml  || exit 1
+make run_small_full_test CACHE="NXF_HOME=/mnt/nextflow_cache/" WORK_DIR="/mnt/work/work" OPTIONS=" -process.cache='lenient' --steps.magAttributes.input=test/bins/small/attributes.tsv " PROFILE="local"  ENTRY="wMagAttributes" PARAMS_FILE=example_params/magAttributes.yml  || exit 1
 make check
