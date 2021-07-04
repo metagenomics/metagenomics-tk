2abf028949d44cdb88faeee693e4914879c23b6a
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2021-07-04 15:06:35 +0000

    fix(magAttributes): checkm header includes now tab separator

--- templates/checkm.sh
+++ templates/checkm.sh
@@ -11,8 +11,9 @@ checkm lineage_set out out/marker &> lineage.log
 checkm analyze -x !{ending} -t !{task.cpus} out/marker . out &> analyze.log
 checkm qa --tab_table -t !{task.cpus} -f checkm.txt out/marker out  &> qa.log
 
-# reformat output files
-echo "SAMPLE\tBIN_ID\tMarker lineage\t# genomes\t# markers\t# marker sets\t0\t1\t2\t3\t4\t5+\tCOMPLETENESS\tCONTAMINATION\tHETEROGENEITY" > checkm_tmp.tsv
+# reformat output files according to magAttributes standard
+echo -e "SAMPLE\tBIN_ID\tMarker lineage\t# genomes\t# markers\t# marker sets\t0\t1\t2\t3\t4\t5+\tCOMPLETENESS\tCONTAMINATION\tHETEROGENEITY" > checkm_tmp.tsv
+sed -i " 2,$ s/\t/.fa\t/"  checkm.txt
 tail -n +2 checkm.txt | sed "s/^/!{sample}\t/g"  >> checkm_tmp.tsv
 echo "PATH" > path.tsv
 tail -n +2 checkm.txt | cut -f 1 | sed "s/$/!{ending}/g" | xargs -I {} readlink -f {} >> path.tsv
