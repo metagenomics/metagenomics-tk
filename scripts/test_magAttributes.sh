c9efceb5b1279439213150897b5a60af625f6c39
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2021-07-04 15:08:46 +0000

    feat(prokka): prokka uses kingdom parameter by using gtdb

--- scripts/test_magAttributes.sh
+++ scripts/test_magAttributes.sh
@@ -1,7 +1,8 @@
-OPTIONS=$@
+OPTIONS=$1
+YAML="${2:-example_params/magAttributes.yml}" 
 make run_small_full_test \
 	WORK_DIR="work" OPTIONS=" $OPTIONS " \
        	PROFILE="local"  ENTRY="wMagAttributes" \
-	PARAMS_FILE=example_params/magAttributes.yml
+	PARAMS_FILE=$YAML
 
 make check
