278cd80f8aebdc1c714b5898eb03cd25a6647501
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2021-10-24 13:55:36 +0200

    fix(renovate): provide renovate loose versioning for docker images

--- renovate-config.js
+++ renovate-config.js
@@ -1,20 +1,22 @@
 module.exports = {
-	  branchPrefix: 'renovate/',
-	  dryRun: false,
-	  username: 'renovate-release',
-	  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-	  onboarding: true,
-	  platform: 'github',
-          repositories: ["pbelmann/meta-omics-toolkit"],
-	  packageRules: [{
-		"regexManagers": [
-		   {
-		      "fileMatch": ["^nextflow.config$"],
-		      "matchStrings": [
-		              "_image* = \"(?<depName>.*?):(?<currentValue>.*?)\""
-		            ],
-		      "datasourceTemplate": "docker"
-		    }
-		    ]
-		  }
-	  ],};
+		  branchPrefix: 'renovate/',
+		  dryRun: false,
+		  username: 'renovate-release',
+		  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+		  onboarding: false,
+		  platform: 'github',
+		  requireConfig: false,
+	          repositories: ["pbelmann/renovate"],
+                  packageRules: [
+                         {  "matchDatasources": ["docker"], versioning: "loose" }
+		  ],
+                  regexManagers: [
+				{
+				   "fileMatch": ["^nextflow.config$"],
+				   "matchStrings": [
+					"_image* = \"(?<depName>.*?):(?<currentValue>.*?)\""
+				],
+				   "datasourceTemplate": "docker"
+				}
+		]
+};
