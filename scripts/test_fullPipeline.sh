eceebe0aea6fb973d12bf96c64d2adaf6110ec7b
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2021-11-11 08:28:19 +0000

    fix(test): use local executor in fullPipeline mode

--- scripts/test_fullPipeline.sh
+++ scripts/test_fullPipeline.sh
@@ -5,7 +5,7 @@ YAML="${2:-example_params/fullPipeline.yml}"
 WORK="${3:-work}"
 make run_small_full_test WORK_DIR=${WORK} \
         PARAMS_FILE=$YAML \
-       	PROFILE="slurm" \
+       	PROFILE="local" \
        	OPTIONS=" $OPTIONS " \
         ENTRY="wPipeline"
 
