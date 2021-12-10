32cd4ea26318a42eea425f0adc456a752367c2e5
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2021-12-10 16:39:11 +0000

    refactor(pipeline): use standard profile instead of local which ist the nextflow default

--- scripts/test_SRA.sh
+++ scripts/test_SRA.sh
@@ -4,6 +4,6 @@ OPTIONS=$1
 YAML="${2:-example_params/SRA.yml}" 
 make run_small_full_test \
 	WORK_DIR="work" OPTIONS=" $OPTIONS " \
-       	PROFILE="local"  ENTRY="wSRATable" \
+       	PROFILE="standard"  ENTRY="wSRATable" \
 	PARAMS_FILE=$YAML
 make check
--- scripts/test_readMapping.sh
+++ scripts/test_readMapping.sh
@@ -5,7 +5,7 @@ YAML="${2:-example_params/readMapping.yml}"
 WORK="${3:-work}"
 make run_small_full_test \
 	WORK_DIR=${WORK} OPTIONS=" $OPTIONS " \
-       	PROFILE="local"  ENTRY="wReadMapping" \
+       	PROFILE="standard"  ENTRY="wReadMapping" \
 	PARAMS_FILE=$YAML
 
 make check
