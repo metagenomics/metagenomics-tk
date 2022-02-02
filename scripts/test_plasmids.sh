daca14245c535ba24f56d87a37c039ba569c248d
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2022-02-02 23:47:53 +0000

    fix(test): use correct profile in plasmid test

--- scripts/test_plasmids.sh
+++ scripts/test_plasmids.sh
@@ -1,9 +1,10 @@
 OPTIONS=$1
 YAML="${2:-example_params/plasmid.yml}"
 WORK="${3:-work}"
+PROFILE="${4:-standard}"
 make run_small_full_test WORK_DIR=${WORK} \
         PARAMS_FILE=$YAML \
-       	PROFILE="standard" \
+       	PROFILE="${PROFILE}" \
        	OPTIONS=" $OPTIONS " \
         ENTRY="wPlasmids"
 make check
