a53b98d8f68e767011bcb14278e07bb7e3fe790b
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2022-03-07 10:47:21 +0000

    feat(readMapping): add samtools upgrade

--- templates/bwa.sh
+++ templates/bwa.sh
@@ -1,5 +1,5 @@
 gunzip -dc !{sample} | bwa mem !{MODE} !{params.steps.readMapping.bwa.additionalParams.bwa_mem} -t !{task.cpus} !{representatives_fasta} - \
       | samtools view -@ !{task.cpus} -S -b - \
-      | samtools sort -l 9 -@ !{task.cpus} - !{sampleID} 
+      | samtools sort -l 9 -@ !{task.cpus} - > !{sampleID}.bam
 
 samtools index !{sampleID}.bam
