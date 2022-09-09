5a3e46931c78436dab3dabbc35065a39e4ac7ee7
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2022-09-09 07:31:54 +0000

    fix(readMapping): use correct params parameter

--- templates/coverm.sh
+++ templates/coverm.sh
@@ -5,14 +5,14 @@ readlink -f !{list_of_representatives} > list.txt
 
 additionalParams = " !{percentIdentity} !{params.steps.readMapping.coverm.additionalParams} " 
 
-coverm genome -t !{task.cpus} !{additionalParams} -b !{mapping} \
+coverm genome -t !{task.cpus} ${additionalParams} -b !{mapping} \
 	--genome-fasta-list list.txt --methods mean --output-file $OUT/mean.tsv && sed -i '1 s| Mean$||' $OUT/mean.tsv  || true
-coverm genome -t !{task.cpus} !{additionalParams} -b !{mapping} \
+coverm genome -t !{task.cpus} ${additionalParams} -b !{mapping} \
 	--genome-fasta-list list.txt --methods trimmed_mean --output-file $OUT/trimmed_mean.tsv && sed -i '1 s| Trimmed Mean$||' $OUT/trimmed_mean.tsv || true
-coverm genome -t !{task.cpus} !{additionalParams} -b !{mapping} \
+coverm genome -t !{task.cpus} ${additionalParams} -b !{mapping} \
 	--genome-fasta-list list.txt --methods count  --output-file $OUT/count.tsv  && sed -i '1 s| Read Count$||' $OUT/count.tsv || true
-coverm genome -t !{task.cpus} !{additionalParams} -b !{mapping} \
+coverm genome -t !{task.cpus} ${additionalParams} -b !{mapping} \
 	--genome-fasta-list  list.txt  --methods rpkm  --output-file $OUT/rpkm.tsv  && sed -i '1 s| RPKM$||' $OUT/rpkm.tsv || true
-coverm genome -t !{task.cpus} !{additionalParams} -b !{mapping} \
+coverm genome -t !{task.cpus} ${additionalParams} -b !{mapping} \
 	--genome-fasta-list list.txt --methods tpm --output-file $OUT/tpm.tsv && sed -i '1 s| TPM$||' $OUT/tpm.tsv || true
 
