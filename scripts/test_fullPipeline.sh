8e98e92e05ad524450730a0e44eb90c1579622c6
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2022-12-10 21:24:05 +0000

    fix(pipeline): rename pipeline to fullpipeline mode

--- scripts/test_fullPipeline.sh
+++ scripts/test_fullPipeline.sh
@@ -4,10 +4,11 @@ OPTIONS=$1
 YAML="${2:-example_params/fullPipeline.yml}"
 WORK="${3:-work}"
 PROFILE="${4:-standard}"
+VERSION="${5:-}"
 make run_small_full_test WORK_DIR=${WORK} \
         PARAMS_FILE=$YAML \
        	PROFILE="$PROFILE" \
        	OPTIONS=" $OPTIONS " \
-        ENTRY="wPipeline"
-
+        ENTRY="wFullPipeline"
+        VERSION="$VERSION" \
 make check
