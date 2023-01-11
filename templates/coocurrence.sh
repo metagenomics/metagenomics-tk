962db09365bf0d5af7ba90b17429de612faa043b
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2023-01-11 22:17:39 +0000

    fix(cooccurrence): add correct formatted binid

--- templates/coocurrence.sh
+++ templates/coocurrence.sh
@@ -1,8 +1,10 @@
 
 # Get BIN_ID, Domain and classification and column and replace semicolon with tab
-cut -f 1,2,5 gtdb.tsv \
-	| sed 's/;/\t/g' \
-	| sed '1 s/classification/DOMAIN\tPHYLUM\tCLASS\tORDER\tFAMILY\tGENUS\tSPECIES/g' > gtdb.fixed.tsv
+cut -f 5 gtdb.tsv | sed 's/;/\t/g' | sed '1 s/classification/DOMAIN\tPHYLUM\tCLASS\tORDER\tFAMILY\tGENUS\tSPECIES/g' > classification.tsv
+
+cut -f 1 gtdb.tsv  | rev | cut -f 2- -d '.' | rev > binId.tsv
+
+paste -d$'\t' classification.tsv binId.tsv > gtdb.fixed.tsv
 
 # Run the actual R coocurrence script
 Rscript /cooccurrence.R -o $(pwd) -m abundance.tsv  -g gtdb.fixed.tsv -c !{task.cpus} !{params.steps.cooccurrence.additionalParams}
