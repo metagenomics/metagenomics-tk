32450ced80ffc4424847542afc99a4d259ce726f
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2023-03-21 11:57:05 +0000

    feat(logs): add simple script to check for errors in log file

--- scripts/check_log.sh
+++ scripts/check_log.sh
@@ -1,5 +1,5 @@
 LOG=$1
 
-cut -d$'\t' -f 4 ${LOG} | sort | uniq -c | tr -s ' ' | sed 's/^ //g' | awk '$1>1' | while read -r line ; do
+cut -d$'\t' -f 6 ${LOG} | sort | uniq -c | tr -s ' ' | sed 's/^ //g' | awk '$1>1' | while read -r line ; do
 	grep "$(echo $line | cut -d ' ' -f 2-)"  ${LOG} | grep -q COMPLETED || (echo "$?"; exit 1);
 done
