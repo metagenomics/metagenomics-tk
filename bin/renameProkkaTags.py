12fbc600f75c871d92531658729472eb8c6e49d6
bosterholz <bosterholz@paderborn.com>
2023-06-06 16:52:44 +0000

    feature(annotation): Another faster locus tag replacement

--- bin/renameProkkaTags.py
+++ bin/renameProkkaTags.py
@@ -12,6 +12,7 @@ import sys
 
 # Read the BIN_PREFIX value from the command line arguments
 BIN_PREFIX = sys.argv[1]
+locus_tag_prefix = ""
 
 # Read the prokka_tags_tmp.tsv file and generate a dictionary with old tags as keys and new tags as values
 with open("prokka_tags_tmp.tsv", "r") as f:
@@ -22,6 +23,7 @@ with open("prokka_tags_tmp.tsv", "r") as f:
         old_tag = fields[2]
         new_tag = "{}_{}_{}".format(BIN_PREFIX, fields[1].replace(fields[0] + '_', ''), fields[2])
         tag_dict[old_tag] = new_tag
+    locus_tag_prefix = fields[2].split("_")[0]
 
 
 # Apply the substitutions to a file
@@ -32,10 +34,13 @@ def apply_substitutions(args):
     # as the whole file content is copied multiple times when using the replace function.
     with open(file_path, "r") as f_in, open(file_path + ".tmp", "w") as f_out:
         for line in f_in:
-            for old_tag, new_tag in tag_dict.items():
-                line = line.replace(old_tag, new_tag)
+            if locus_tag_prefix in line:
+                # Find the exact locus tag with its underscore and digits
+                locus_tag = re.search(r"{}_\d+".format(locus_tag_prefix), line).group(0)
+                line = line.replace(locus_tag, tag_dict[locus_tag])
             f_out.write(line)
     os.rename(file_path + ".tmp", file_path)
+
 
 # Find all files with the specified extensions and apply the substitutions to each file separately
 file_paths = []
