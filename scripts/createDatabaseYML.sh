28c2f32f818a2d9e8eb3d6fa3f04adf61a477acd
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2024-10-14 10:04:29 +0000

    fix(tests): use correct bash statement for skipping tests

--- scripts/createDatabaseYML.sh
+++ scripts/createDatabaseYML.sh
@@ -12,27 +12,27 @@ yamlToTest="${9}"
 yamlKey="${10}"
 skipTests="${11}"
 
-if [[ -z "$skipTests" ]] || [[ $skipTests != "extracted" ]]; then
+if [[ -z "$skipTests" ]] || [[ $skipTests != *"extracted"* ]]; then
 database="{extractedDBPath: ${extractedPath} }" \
 	yq ${yamlKey} ${yamlToTest} > ${basePath}/extractedPath.yml
 fi
 
-if [[ -z "$skipTests" ]] || [[ $skipTests != "https" ]]; then
+if [[ -z "$skipTests" ]] || [[ $skipTests != *"https"* ]]; then
 database="{download: {source: ${https}, md5sum: ${md5sum}} }" \
 	yq ${yamlKey} ${yamlToTest} > ${basePath}/https.yml
 fi
 
-if [[ -z "$skipTests" ]] || [[ $skipTests != "compressed" ]]; then
+if [[ -z "$skipTests" ]] || [[ $skipTests != *"compressed"* ]]; then
 database="{download: {source: ${compressedPath}, md5sum: ${md5sum}} }" \
 	yq ${yamlKey} ${yamlToTest} > ${basePath}/compressedPath.yml
 fi
 
-if [[ -z "$skipTests" ]] || [[ $skipTests != "s3File" ]]; then
+if [[ -z "$skipTests" ]] || [[ $skipTests != *"s3File"* ]]; then
 database="{download: {source: ${s3File}, md5sum: ${md5sum}, s5cmd: { params: ${s5cmdCommand} }}}" \
 	yq ${yamlKey} ${yamlToTest} > ${basePath}/s3File.yml
 fi
 
-if [[ -z "$skipTests" ]] || [[ $skipTests != "s3Directory" ]]; then
+if [[ -z "$skipTests" ]] || [[ $skipTests != *"s3Directory"* ]]; then
 database="{download: {source: ${s3Directory}, md5sum: ${md5sum}, s5cmd: { params: ${s5cmdCommand} }}}" \
 	yq ${yamlKey} ${yamlToTest} > ${basePath}/s3Directory.yml
 fi
