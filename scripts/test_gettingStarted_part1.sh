fce2d21d39b9fafed2d3cc9e1507cbc4d003116a
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2025-01-20 11:24:45 +0100

    fix(tests): use correct http github file path

--- scripts/test_gettingStarted_part1.sh
+++ scripts/test_gettingStarted_part1.sh
@@ -2,10 +2,10 @@
 NXF_HOME=$PWD/.nextflow NXF_VER=23.10.0 nextflow run metagenomics/metagenomics-tk -work-dir $(pwd)/work \
     -profile slurm \
     -entry wFullPipeline \
-    -params-file https://github.com/metagenomics/metagenomics-tk/blob/master/default/fullPipeline_illumina_nanpore_getting_started_part1.yml \
+    -params-file  https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/feat/getting_started_guide/default/fullPipeline_illumina_nanpore_getting_started_part1.yml \
     --s3SignIn false \
     --scratch /vol/scratch \
     --databases /vol/scratch/databases \
     --output output \
-    --input.paired.path https://github.com/metagenomics/metagenomics-tk/blob/master/test_data/fullPipeline/reads_split.tsv
+    --input.paired.path https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/test_data/fullPipeline/reads_split.tsv 
 
