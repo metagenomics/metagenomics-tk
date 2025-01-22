d9b6dacabae553dd28108507e3e8f75b4773fd6b
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2025-01-22 13:59:26 +0000

    refactor(cooccurrence): use aggregated output from Utils class

--- lib/Utils.groovy
+++ lib/Utils.groovy
@@ -47,6 +47,13 @@ class Utils {
           module.version.patch
   }
 
+  static String getAggregatedOutput(runid, tool, module, filename){
+    return "AGGREGATED" + '/' +  tool + '/' + module.name + '/' + 
+         module.version.major + "." +  
+         module.version.minor + "." +  
+         module.version.patch +  
+         '/' + tool + '/' + filename
+  }
 
   static String getBeforeScript(script, image){
     if(script.isEmpty()){
