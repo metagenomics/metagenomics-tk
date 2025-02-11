fba59d5d5ad5b4de1600580226ee1e8e93a098ee
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2025-02-11 08:54:08 +0100

    docs(gettingStarted): updated snippets cutout

--- scripts/test_quickstart.sh
+++ scripts/test_quickstart.sh
@@ -4,10 +4,11 @@ NXF_VER=23.10.0 nextflow run metagenomics/metagenomics-tk \
 	  -profile standard \
 	  -entry wFullPipeline \
 	  -params-file default/quickstart.yml \
+	  --logDir logs \
 	  --s3SignIn false \
 	  --scratch false \
 	  --output output \
 	  --databases $(pwd)/databases
 
 
-make check LOG_DIR=$(pwd)/work/logs
+make check LOG_DIR=$(pwd)/logs
