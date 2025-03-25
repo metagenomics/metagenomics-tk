d2e57d3d08765dde10128c874a18eed716887339
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2025-03-25 22:14:46 +0100

    feat(tutorial1): add tests for bin evaluation part

--- scripts/tutorials/tutorial1/test_bin_quality.sh
+++ scripts/tutorials/tutorial1/test_bin_quality.sh
@@ -1,13 +1,14 @@
 # This file will be referenced on the online wiki
 
+cd ~/mgcourse/
+
 NXF_VER=24.10.4 nextflow run metagenomics/metagenomics-tk \
 	  -profile standard \
 	  -params-file https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/master/default/tutorials/tutorial1/fullpipeline_bin_quality.yml \
 	  -ansi-log false \
 	  -entry wFullPipeline \
 	  -resume \
-	  --logDir logs_bin_quality \
-	  --s3SignIn false \
-	  --scratch false
+    --input.paired.path https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/master/test_data/tutorials/tutorial1/reads.tsv \
+	  --logDir logs_bin_quality 
 
 make check LOG_DIR=$(pwd)/logs_bin_quality
