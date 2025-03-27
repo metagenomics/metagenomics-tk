c140cc0c4342c7df30f0427ae8d197d4d1c975ce
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2025-03-27 09:28:37 +0100

    feat(tutorial1): add tests for annotation

--- scripts/tutorials/tutorial1/test_annotation.sh
+++ scripts/tutorials/tutorial1/test_annotation.sh
@@ -1,13 +1,14 @@
 # This file will be referenced on the online wiki
 
+cd ~/mgcourse/
+
 NXF_VER=24.10.4 nextflow run metagenomics/metagenomics-tk \
 	  -profile standard \
 	  -params-file https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/master/default/tutorials/tutorial1/fullpipeline_annotation.yml \
 	  -ansi-log false \
 	  -entry wFullPipeline \
 	  -resume \
-	  --logDir logs_annotation \
-	  --s3SignIn false \
-	  --scratch false
+    --input.paired.path https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/master/test_data/tutorials/tutorial1/reads.tsv \
+	  --logDir logs_annotation 
 
 make check LOG_DIR=$(pwd)/logs_annotation
