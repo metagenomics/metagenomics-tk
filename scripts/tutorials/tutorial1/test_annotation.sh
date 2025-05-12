67d1f9097a88371c76ed80e627351dd9aef63499
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2025-05-12 07:55:58 +0000

    feat(nextflow): upgrade to 25.04.0

--- scripts/tutorials/tutorial1/test_annotation.sh
+++ scripts/tutorials/tutorial1/test_annotation.sh
@@ -2,7 +2,7 @@
 
 cd ~/mgcourse/
 
-NXF_VER=24.10.4 nextflow run metagenomics/metagenomics-tk \
+NXF_VER=25.04.0 nextflow run metagenomics/metagenomics-tk \
 	  -profile standard \
 	  -params-file https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/master/default/tutorials/tutorial1/fullpipeline_annotation.yml \
 	  -ansi-log false \
