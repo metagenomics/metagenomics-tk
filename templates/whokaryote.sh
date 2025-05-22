a8cc3d1563a2d307cb73ee10cdfa272db154c8ec
Peter Belmann <pbelmann@users.noreply.github.com>
2025-05-22 16:20:18 +0200

    feat(annotation): whokaryote integrated (#392)
    * feat(docker): add prokka ability to output prodigal raw output
    
    * feat(annotation): add whokaryote for seperation between eukaryotes and prokaryotes
    
    * fix(annotation):  add columns to whokaryote output
    
    * doc(annotation): add whokaryote to annotation doc
    
    * feat(annotation): whokaryote should increase memory in case of error
    
    * fix(toolit): remove unnecessary files
    
    * feat(config): upgrade export and annotation module
    
    * doc(annotation): explain difference between prodigal and prokka gff

---
+++ templates/whokaryote.sh
@@ -0,0 +1,29 @@
+gunzip -c !{gff} > unzipped.gff
+
+whokaryote.py --contigs !{fasta} --threads !{task.cpus} --gff unzipped.gff --outdir output !{params.steps.annotation.whokaryote.additionalParams} 
+
+# Add sample and bin id column
+addColumns() {
+	local input_file="$1"
+	local output_file="$2"
+
+        sed -e "1s/^/SAMPLE\tBIN_ID\t/" \
+  	  -e "2,$ s/^/!{sample}\t/" \
+	  -e "2,$ s/^/!{binID}\t/" $input_file > $output_file 
+}
+
+# Add missing column header
+sed -i '1i CONTIG' output/eukaryote_contig_headers.txt output/prokaryote_contig_headers.txt
+
+# Output tsv instead of csv
+sed -i 's/,/\t/g' output/featuretable.csv
+
+addColumns output/featuretable.csv !{binID}_featuretable.tsv
+addColumns output/featuretable_predictions_T.tsv !{binID}_featuretable_predictions_T.tsv
+addColumns output/eukaryote_contig_headers.txt !{binID}_eukaryote_contig_headers.tsv
+addColumns output/prokaryote_contig_headers.txt !{binID}_prokaryote_contig_headers.tsv
+
+# In case Tiara is used
+if [ -f output/tiara_pred.txt ]; then
+	addColumns output/tiara_pred.txt !{binID}_tiara_pred.tsv
+fi
