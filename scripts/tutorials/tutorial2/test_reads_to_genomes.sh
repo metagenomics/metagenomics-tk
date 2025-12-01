5657ce3ba6fd350a6c13cad9e426d02a8a219e4f
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2025-12-01 14:05:58 +0000

    feat(tutorial2): set correct path for testing

--- scripts/tutorials/tutorial2/test_reads_to_genomes.sh
+++ scripts/tutorials/tutorial2/test_reads_to_genomes.sh
@@ -1,14 +1,14 @@
 # This file will be referenced on the online wiki
 
-cd ~/mgcourse/
+#cd ~/mgcourse/
 
 NXF_VER=25.04.2 nextflow run metagenomics/metagenomics-tk \
 	  -profile standard \
-	  -params-file https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/master/default/tutorials/tutorial2/fullPipeline_reads_to_genomes.yml \
+	  -params-file https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/doc/exhibition-day/tutorial/default/tutorials/tutorial2/fullPipeline_reads_to_genomes.yml \
 	  -ansi-log false \
 	  -entry wFullPipeline \
 	  -resume \
-	  --input.paired.path https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/master/test_data/tutorials/tutorial2/samples.tsv \
+	  --input.SRA.S3.path https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/doc/exhibition-day/tutorial/test_data/tutorials/tutorial2/samples.tsv \
 	  --logDir logs_reads_to_genomes 
 
 make check LOG_DIR=$(pwd)/logs_reads_to_genomes
