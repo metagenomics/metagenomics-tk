f6803b3c66b6007c69b08c418e7b557c0129c391
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2026-02-27 16:19:50 +0100

    remove settings tests temporary

--- scripts/test_settings.sh
+++ scripts/test_settings.sh
@@ -8,13 +8,14 @@ PROFILE="${4:-standard}"
 VERSION="${5:-}"
 PRESET="${6:-}"
 LOG_DIR="${WORK}/logs"
-make run_small_full_test WORK_DIR=${WORK} \
-	        PARAMS_FILE="$YAML" \
-		PRESET="${PRESET}" \
-		LOG_DIR=${LOG_DIR} \
-		PROFILE="$PROFILE" \
-	        OPTIONS=" $OPTIONS " \
-	        ENTRY="${ENTRY}" \
-	        VERSION="$VERSION" 
 
-make check LOG_DIR=${LOG_DIR}
+#make run_small_full_test WORK_DIR=${WORK} \
+#	        PARAMS_FILE="$YAML" \
+#		PRESET="${PRESET}" \
+#		LOG_DIR=${LOG_DIR} \
+#		PROFILE="$PROFILE" \
+#	        OPTIONS=" $OPTIONS " \
+#	        ENTRY="${ENTRY}" \
+#	        VERSION="$VERSION" 
+
+#make check LOG_DIR=${LOG_DIR}
