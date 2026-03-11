f3a061e95ddbea90b218377c39020daa85eecce0
Proton-NTA <jan.tawakol@uni-bielefeld.de>
2026-03-11 09:14:11 +0000

    chore(fragmentRecruitment): replace shell with script

--- templates/mashScreen.sh
+++ templates/mashScreen.sh
@@ -1,13 +1,13 @@
 set -o pipefail
 
-mash screen -p !{task.cpus} !{params?.steps?.fragmentRecruitment?.mashScreen?.additionalParams.mashScreen} !{sketch} !{pairedReads} !{singleReads} \
-	| sed "s/^/!{sample}\t/g" >> mash_screen_temp.tsv
+mash screen -p ${task.cpus} ${params?.steps?.fragmentRecruitment?.mashScreen?.additionalParams.mashScreen} ${sketch} ${pairedReads} ${singleReads} \\
+	| sed "s/^/${sample}\\t/g" >> mash_screen_temp.tsv
 
 set +o pipefail
 
-awk -v dist=!{params?.steps?.fragmentRecruitment?.mashScreen?.mashDistCutoff} \
-    -v hash=!{params?.steps?.fragmentRecruitment?.mashScreen?.mashHashCutoff} \
-    '{ if ($2 >= dist && $3 >= hash) print $7 }' <(cat mash_screen_temp.tsv | sed "s/\//\t/g") > selected_genomes_temp.tsv
+awk -v dist=${params?.steps?.fragmentRecruitment?.mashScreen?.mashDistCutoff} \\
+    -v hash=${params?.steps?.fragmentRecruitment?.mashScreen?.mashHashCutoff} \\
+    '{ if (\$2 >= dist && \$3 >= hash) print \$7 }' <(cat mash_screen_temp.tsv | sed "s/\\//\\t/g") > selected_genomes_temp.tsv
 
 
 if [ -s selected_genomes_temp.tsv ]
