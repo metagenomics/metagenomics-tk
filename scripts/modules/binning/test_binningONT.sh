1089264aef75442155ea256eaacb6ec816ca4dc6
Peter Belmann <pbelmann@cebitec.uni-bielefeld.de>
2026-05-18 09:51:54 +0000

    fix(binning): tests should only include inputs for metabat

--- scripts/modules/binning/test_binningONT.sh
+++ scripts/modules/binning/test_binningONT.sh
@@ -9,9 +9,6 @@ NXF_VER=25.10.4 nextflow run metagenomics/metagenomics-tk \
           --steps.binningONT.input.reads https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/master/test_data/binningONT/samplesONT.tsv \
           --steps.binningONT.input.contigs https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/master/test_data/binningONT/assemblyONT.tsv \
           --steps.binningONT.input.quality https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/master/test_data/binningONT/quality.tsv \
-          --steps.binningONT.input.graph https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/master/test_data/binningONT/graph.tsv \
-          --steps.binningONT.input.headerMapping https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/master/test_data/binningONT/headerMapping.tsv \
-          --steps.binningONT.input.assemblyInfo https://raw.githubusercontent.com/metagenomics/metagenomics-tk/refs/heads/master/test_data/binningONT/headerMapping.tsv \
 	  --logDir logs_binningONT 
 
 make check LOG_DIR=$(pwd)/logs_binningONT
